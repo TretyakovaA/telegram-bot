2892a8463821ff32fad118aed1e26a9867491bb2
TretyakovaA <putinvivo@bk.ru>
2023-05-13 15:02:42 +0300

    course work is done

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -7,11 +7,13 @@ import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.model.NotificationTasks;
+import pro.sky.telegrambot.repository.NotificationTasksRepository;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,8 +26,15 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    @Autowired
-    private TelegramBot telegramBot;
+    private Pattern pattern = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
+    private final TelegramBot telegramBot;
+
+    private final NotificationTasksRepository notificationTasksRepository;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot, NotificationTasksRepository notificationTasksRepository) {
+        this.telegramBot = telegramBot;
+        this.notificationTasksRepository = notificationTasksRepository;
+    }
 
     @PostConstruct
     public void init() {
@@ -38,15 +47,16 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             logger.info("Processing update: {}", update);
             // Process your updates here
             long chatId = update.message().chat().id();
-            if (update.message().text().equals("/start")){
+            if (update.message().text().equals("/start")) {
                 SendResponse response = telegramBot.execute(new SendMessage(chatId, "Приветствую! Пожалуйста, введите сообщение-напоминалку в формате: дд.мм.гггг чч:мм текст напоминалки"));
             } else {
-                Pattern pattern = Pattern.compile ("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
                 Matcher matcher = pattern.matcher(update.message().text());
                 if (matcher.matches()) {
                     String date = matcher.group(1);
                     String item = matcher.group(3);
                     NotificationTasks task = new NotificationTasks(update.message().chat().id(), item, LocalDateTime.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")));
+                    notificationTasksRepository.save(task);
+                    SendResponse response = telegramBot.execute(new SendMessage(chatId, "Спасибо, обязательно напомним!"));
                 } else {
                     SendResponse response = telegramBot.execute(new SendMessage(chatId, " Ошибка! Введите сообщение в формате: дд.мм.гггг чч:мм текст напоминалки"));
                 }
@@ -54,6 +64,14 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+    @Scheduled(cron = "0 0/1 * * * *")
+    public void testNotifications() {
+        List<NotificationTasks> allTasks = notificationTasksRepository.findNotificationTasksByNotificationTime(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES));
+        for (NotificationTasks task: allTasks){
+            SendResponse response = telegramBot.execute(new SendMessage(task.getChatId(), task.getNotification()));
+        }
     }
 
 
--- src/main/java/pro/sky/telegrambot/repository/NotificationTasksRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationTasksRepository.java
@@ -3,5 +3,10 @@ package pro.sky.telegrambot.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pro.sky.telegrambot.model.NotificationTasks;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface NotificationTasksRepository extends JpaRepository<NotificationTasks, Long> {
+
+    List<NotificationTasks> findNotificationTasksByNotificationTime(LocalDateTime notificationTime);
 }
